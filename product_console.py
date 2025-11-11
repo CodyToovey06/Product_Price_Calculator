f580d5cae2e45c29ca6a907e9bebbb24b7465837
Cody Toovey <ctoov9@eq.edu.au>
2025-11-11 10:30:09 +1000

    Add function docstrings to product_console.py

--- product_console.py
+++ product_console.py
@@ -8,14 +8,14 @@ FILENAME = "products.json"
 
 
 def main():
-    """"""
+    """Program for displaying products name and price from file."""
     products = load_products()
     for product in products:
         print(product)
 
 
 def load_products():
-    """"""
+    """Load products from json file."""
     products = []
     with open(FILENAME) as in_file:
         records = json.load(in_file)
